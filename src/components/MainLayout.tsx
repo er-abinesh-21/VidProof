0619317e996d30adb4f041cb43f6e09bc607e253
XxEr_Abinesh_MrxX <147540638+er-abinesh-21@users.noreply.github.com>
2025-07-06 18:58:18 +0530

    Update MainLayout.tsx

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -32,7 +32,6 @@ const MainLayout = () => {
         <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 overflow-auto">
           <Outlet />
         </main>
-        <MadeWithDyad />
       </div>
     </div>
   );
